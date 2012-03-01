4fec67198b1537ca45e4fdb55f805456c0486161
javovo <javovo@gmail.com>
2012-03-01 12:59:32 -0500

    Integration tests for routes added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                            :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -20,24 +25,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-     it "should have the right title" do
+
+    it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-     it "should have the right title" do
+
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
   end
 end
