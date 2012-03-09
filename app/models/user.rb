5d65e35d5ef1ff3c8acd1f4dcf4d62985ceb711d
javovo <javovo@gmail.com>
2012-03-09 08:27:19 -0500

    At Figure 7.3 in tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,22 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
+
+before_save :encrypt_password
+
+ # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+  end
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,5 +98,8 @@ describe User do
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
   end
 end
