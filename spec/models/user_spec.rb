14f0ee3a627f8097a8868f0f01953eee7a986ccb
javovo <javovo@gmail.com>
2012-03-09 08:28:10 -0500

    At Figure 7.3 in tutorial (forgot to save this file)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,5 +101,15 @@ describe User do
     it "should set the encrypted password" do
       @user.encrypted_password.should_not be_blank
     end
+     describe "has_password? method" do
+
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end    
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end 
+    end
   end
 end
